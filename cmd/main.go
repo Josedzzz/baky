d53bbba5920d545111d8e1adb53df939966ecea4
Joki <josedavidamayar@gmail.com>
2026-04-21 11:22:14 -0500

    feat: call temp dir cleanup on app startup

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/Josedzzz/baky/internal/backup"
+	"github.com/Josedzzz/baky/internal/restore"
 	"github.com/Josedzzz/baky/internal/tui"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
+	// Clean up old temporary restore directories (older than 7 days)
+	if err := restore.CleanupOldTempDirs(7 * 24 * time.Hour); err != nil {
+		fmt.Printf("Warning: Could not cleanup old temp directories: %v\n", err)
+	}
+
 	// Start background backup engine
 	if err := backup.StartWatcher(); err != nil {
 		fmt.Printf("Warning: Could not start file watcher: %v\n", err)
